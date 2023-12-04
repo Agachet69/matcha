1398dfd263a616d9fb839b4c061383b1df776936
Alexis GACHET <agachet@amiltone.com>
2023-12-04 14:10:07 +0100

    add input

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,23 @@
+import { useState } from "react"
+
 const Login = () => {
+
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  function tryLog() {
+    console.log(name);
+    console.log(password);
+  }
+
     return (
-        <div>Login</div>
+        <div>
+          <p>Login</p>
+            <input type="text" placeholder='name' onChange={e => setName(e.currentTarget.value)} value={name} />
+            <input type="password" placeholder='password' onChange={e => setPassword(e.currentTarget.value)} value={password} />
+            <button onClick={tryLog}> Connexion </button>
+            {name}
+        </div>
     )
 }
 
