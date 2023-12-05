ced547bd9e71d12b721489a09c25dd3f69ab67b6
Alexis <gachet.alexisfc@gmail.com>
2023-12-06 00:55:24 +0100

    design login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,63 +3,66 @@ import { useDispatch, useSelector } from "react-redux";
 import { isAuth, selectAuth } from "../store/slices/authSlice";
 import { useNavigate } from "react-router-dom";
 import "../styles/login.css";
+import { KeyIcon, UserIcon } from "../components/icons/Icons";
 
 const Login = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
 
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const auth = useSelector(selectAuth);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const auth = useSelector(selectAuth);
 
-  function tryLog() {
-    console.log(auth);
-    dispatch(isAuth(true));
-    console.log(auth);
+    function tryLog() {
+        console.log(auth);
+        dispatch(isAuth(true));
+        console.log(auth);
 
-    console.log(name);
-    console.log(password);
-    navigate("/profil");
-  }
+        console.log(name);
+        console.log(password);
+        navigate("/profil");
+    }
 
-  return (
-    <div className="loginContainer">
-      <header>
-        <h2>Matcha ❤</h2>
-      </header>
-      <main>
-        <h3> Welcome to login! </h3>
-        <p> Please enter your details </p>
-        <form>
-          <div className="loginInput">
-            <label> username </label>
-            <input
-              type="text"
-              onChange={(e) => setUsername(e.currentTarget.value)}
-              value={username}
-            />
-            <img src="/icons/key.svg" alt="icône d'utilisateur" />
-          </div>
-          <div className="loginInput">
-            <label> Password</label>
-            <input
-              type="password"
-              onChange={(e) => setPassword(e.currentTarget.value)}
-              value={password}
-            />
-            <img src="/icons/user.svg" alt="icône d'utilisateur" />
-          </div>
-          <button onClick={tryLog}> Log in </button>
-          <p> Forgot your password? </p>
-          <p> or </p>
-          <p>
-            {" "}
-            Need an account? <span>sign-up here</span>
-          </p>
-        </form>
-      </main>
-    </div>
-  );
+    return (
+        <div className="loginContainer">
+            <header>
+                <h2>Matcha ❤</h2>
+            </header>
+            <main>
+                <h3> Welcome to login! </h3>
+                <p> Please enter your details </p>
+                <section>
+                <form>
+                    <div className="loginInput">
+                        <input
+                            type="text"
+                            placeholder=" "
+                            onChange={(e) => setUsername(e.currentTarget.value)}
+                            value={username}
+                            />
+                        <label> Username </label>
+                        <UserIcon />
+                    </div>
+                    <div className="loginInput">
+                        <input
+                            type="password"
+                            placeholder=" "
+                            onChange={(e) => setPassword(e.currentTarget.value)}
+                            value={password}
+                            />
+                            <label> Password</label>
+                            <KeyIcon />
+                    </div>
+                </form>
+                <div className="bottomForm">
+                    <p> Forgot password? </p>
+                    <p onClick={() => navigate('/register')}> Sign-up </p>
+                </div>
+                </section>
+                <button className="loginButton" onClick={tryLog}> Log in </button>
+            </main>
+        </div>
+    );
 };
 
 export default Login;
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,19 @@
 const Register = () => {
     return (
-        <div>Register</div>
+        <div>
+            <h1>Register</h1>
+            <p>pseudo</p>
+            <p>mdp</p>
+            <p>nom</p>
+            <p>prénom</p>
+            <p>genre</p>
+            <p>sexualité</p>
+            <p>description</p>
+            <p>position</p>
+            <p>age</p>
+            <p>email</p>
+            <p>photos</p>
+        </div>
     )
 }
 
